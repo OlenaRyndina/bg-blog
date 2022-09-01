797a573deab144ad85585b0d1192dd8dc86638d3
Olena Ryndina <olenaryndina1111@gmail.com>
2022-09-01 11:54:59 +0300

    build routing and guards

---
+++ src/app/routing/website/routing/admin/routing/dashboard/dashboard.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+
+
+@NgModule({
+    declarations: [
+        DashboardComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: DashboardComponent
+            }
+        ])
+    ]
+})
+
+export class DashboardModule { }
---
+++ src/app/routing/website/routing/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+
+
+@NgModule({
+    declarations: [
+        HomePageComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                pathMatch: 'full',
+                component: HomePageComponent
+            }
+        ])
+    ]
+})
+export class HomeModule { }

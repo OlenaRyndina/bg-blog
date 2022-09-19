908206fa9f0073d50927e84b6fb94b8175fd12e9
Olena Ryndina <olenaryndina1111@gmail.com>
2022-09-19 15:12:02 +0300

    admin panel layout

---
+++ src/app/view/admin-footer-block/blocks/admin-footer-block/admin-footer-block.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-footer-block',
+  templateUrl: './admin-footer-block.component.html',
+  styleUrls: ['./admin-footer-block.component.scss']
+})
+export class AdminFooterBlockComponent {
+
+
+}
